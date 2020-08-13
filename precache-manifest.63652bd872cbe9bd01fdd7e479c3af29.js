3392148b82b0a6b3c1a29608b7d13eacf3ec0ea5
Your Name <duytrankc1@gmail.com>
2020-08-13 23:14:04 +0700

    Updates

---
+++ precache-manifest.63652bd872cbe9bd01fdd7e479c3af29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f9d71bf996f82aa2c8840499cfc40a",
+    "url": "/dooftsaf.manager/index.html"
+  },
+  {
+    "revision": "29c1168d1990f8001683",
+    "url": "/dooftsaf.manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e84cca681af56d122749",
+    "url": "/dooftsaf.manager/static/css/main.c77f04b3.chunk.css"
+  },
+  {
+    "revision": "29c1168d1990f8001683",
+    "url": "/dooftsaf.manager/static/js/2.282ac340.chunk.js"
+  },
+  {
+    "revision": "7eaf41a8e2b1726190789a136eda544e",
+    "url": "/dooftsaf.manager/static/js/2.282ac340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84cca681af56d122749",
+    "url": "/dooftsaf.manager/static/js/main.9dabfd82.chunk.js"
+  },
+  {
+    "revision": "8a13e9369bc7e3a1cef0",
+    "url": "/dooftsaf.manager/static/js/runtime-main.459c0d56.js"
+  }
+]);
